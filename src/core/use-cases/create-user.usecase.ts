eae17d082965715a873231962163ea5ebfaead61
Pedro Henrique Antunes Pinto <pedro.pinto@amo.delivery>
2026-01-03 18:00:59 -0300

    feat: added repository scope

--- src/core/use-cases/create-user.usecase.ts
+++ src/core/use-cases/create-user.usecase.ts
@@ -1,11 +1,28 @@
+import { nanoid } from 'nanoid';
 import { User } from '../entities/user.entity';
 import { UserRepositoryPort } from '../ports/user.repository.port';
 
 export class CreateUserUseCase {
-  constructor(private readonly userRepository: UserRepositoryPort) {}
+  constructor(private readonly userRepository: UserRepositoryPort) { }
 
-  async execute(name: string, email: string): Promise<User> {
-    const user = new User(Date.now(), name, email);
+  async execute(
+    name: string,
+    email: string,
+    phone: string,
+    fixed_salary: number,
+    total_balance: number,
+    salary_payment_date: Date
+  ): Promise<User> {
+    const user = new User(
+      1,
+      name,
+      email,
+      phone,
+      fixed_salary,
+      total_balance,
+      salary_payment_date
+    );
+
     return this.userRepository.create(user);
   }
 }
